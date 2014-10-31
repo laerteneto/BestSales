fe096a894c093daff58f8007e1ce2df49dc76817
laerteneto <laerte-m@hotmail.com>
2014-10-30 20:26:39 -0500

    add script to creation of automatic inicial datas

---
+++ Database/script_datas.sql
@@ -0,0 +1,12 @@
+--Products
+INSERT INTO produtos VALUES (1, 'iphone6+', 250, 750, 'Black - 16GB');
+INSERT INTO produtos VALUES (2, 'Galaxy S5', 200, 700, 'Black - 32GB');
+INSERT INTO produtos VALUES (3, 'Xperia Z3', 300, 700, 'White - 64GB');
+INSERT INTO produtos VALUES (4, 'Moto x', 180, 550, 'Gold - 16GB');
+
+--Customer
+INSERT INTO clientes VALUES (1, 'Laerte Mello', '1111111111', '4026987589', '1N 1th st', '10/30/2014');
+INSERT INTO clientes VALUES (2, 'Lance', '2222222222', '4025879685', '2N 2th st', '10/30/2014');
+INSERT INTO clientes VALUES (3, 'Hu', '3333333333', '4026987894', '3N 3th st', '10/30/2014');
+
+--done
